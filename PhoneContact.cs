ad69710d345027bb82fa3c4c6f4b2091c9f3c1c4
kchrosci <kamil.chroscicki.stud@pw.edu.pl>
2021-10-07 21:29:10 +0200

    Initial commit

---
+++ PhoneContact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+	class PhoneContact
+	{
+		private string _phoneName;
+		private string _phoneNumber;
+		public string PhoneName 
+		{
+			get
+			{
+				return _phoneName;
+			}
+			set
+			{
+				_phoneName = value;
+			} 
+		}		
+		public string PhoneNumber
+		{
+			get
+			{
+				return _phoneNumber;
+			}
+			set
+			{
+				_phoneNumber = value;
+			} 
+		}
+		public PhoneContact(string phoneName, string phoneNumber)
+		{
+			PhoneName = phoneName;
+			PhoneNumber = phoneNumber;		
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PhoneBook
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Phone Book program!");
+			Information();
+			int decision = int.Parse(Console.ReadLine());
+			while (decision != 0) 
+			{
+				PhoneContact phoneContact = new PhoneContact("Jano", "123123123131");
+
+
+				Information();
+				decision = int.Parse(Console.ReadLine());
+			}
+		}
+		public static void Information()
+		{
+			Console.WriteLine("Please input a number depending on your choice: ");
+			Console.WriteLine("1. Add new contact");
+			Console.WriteLine("2. Display a contact depending on phone number");
+			Console.WriteLine("3. Show all the numbers");
+			Console.WriteLine("4. Find all the numbers for specific name");
+			Console.WriteLine("0. Exit the program");
+		}
+	}
+}
